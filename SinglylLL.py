6cb09c612065b68cf2af4f32e6c3f7fded40d3bd
Deva <devakrishnasatakuri@gmail.com>
2025-12-03 19:05:17 +0530

    Singly Linked List Built a sll with a Naive method

---
+++ SinglylLL.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+class Singlylinkedlist:
+    def __init__(self):
+        self.head= None
+    def build_list(self):
+        first=Node(10)
+        second=Node(20)
+        third=Node(30)
+        fourth=Node(40)
+
+        first.next=second
+        second.next=third
+        third.next=fourth
+
+        self.head=first
+sll=Singlylinkedlist()
+sll.build_list()
+print(sll.head.data )
+
+
