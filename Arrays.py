80ca667b0dc028edd07a2cb5740696e1632ac1d7
Deva <devakrishnasatakuri@gmail.com>
2025-12-10 18:44:01 +0530

    Arrays > Creating a 1-d and 2-d arrays and printing them. > Deleting an element in an array. > Sorting an array in ascending and descending order with a temporary variable. >

--- Arrays.py
+++ Arrays.py
@@ -7,7 +7,7 @@ num=int(num)
 for i in range(num):
     element=input()
     arr.append(element)
-print("The element in array are:")
+print("The elements in array are:")
 for i in range (num):
     print(arr[i],end="")
 
@@ -20,4 +20,54 @@ for row in range(r_num):
     for col in range(r_num):
         twod_arr[row][col]=row*col
 print(twod_arr)
+
+#DELETING AN ELEMENT IN AN ARRAY
+print(end="Size of the array : ")
+tot=int(input())
+arr=[]
+print(end="Enter" +str(tot)+ "elements : ")
+for i in range(tot):
+    arr.append(input())
+print("\nValue to be deleted : ")
+val=input()
+if val in arr:
+    arr.remove(val)
+    print("\nTHe new array is : ")
+    for i in range(tot-1):
+        print(end=arr[i]+" ")
+else:
+    print("\nElement doesn't exist in the array")
+
+
+#SORTING OF AN ARRAY (ASCENDING)
+arr=[10,40,30,60,55]
+temp=0
+
+print("Elements of original array : ")
+for i in range(0, len(arr)):
+    print(arr[i], end=" ")
+for i in range(0, len(arr)):
+    for j in range(i+1, len(arr)):
+        if(arr[i]>arr[j]):
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+print();
+print('Elements sorted in ascending order: ')
+for i in range(0, len(arr)):
+    print(arr[i], end=" ")
+
+#SORTING OF AN ARRAY (DESCENDING)
+for i in range(0, len(arr)):
+    for j in range(i+1, len(arr)):
+        if(arr[i]<arr[j]):
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp
+print();
+print("Elements sorted in descending order : ")
+for i in range(0, len(arr)):
+    print(arr[i], end=" ")
+
+#SORTING OF AN ARRAY WITHOUT ASSIGNING A TEMPORARY VARIABLE
 
